9da3a8074da9cb4d9137448659968e4f0cc98550
km <35552602+catnap7777@users.noreply.github.com>
2020-09-21 15:43:06 -0500

    added text colors

--- MAD157Module6TabBarPicker2KarenM/ViewController.swift
+++ MAD157Module6TabBarPicker2KarenM/ViewController.swift
@@ -32,13 +32,19 @@ class ViewController: UIViewController {
         
         //.. alert action for each "button" on the alert
         let okAction = UIAlertAction(title: "OK", style: .default, handler: { action -> Void in
-            self.labelResult.text = "OK"         })
+            self.labelResult.text = "OK"
+            self.labelResult.textColor = UIColor.blue
+        })
         
         let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: { action -> Void in
-            self.labelResult.text = "Cancel"         })
+            self.labelResult.text = "Cancel"
+            self.labelResult.textColor = UIColor.black
+        })
         
         let destroyAction = UIAlertAction(title: "Destroy", style: .destructive, handler: { action -> Void in
-            self.labelResult.text = "Destroy"         })
+            self.labelResult.text = "Destroy"
+            self.labelResult.textColor = UIColor.red
+        })
         
         //.. add the "buttons" to the alert
         alert.addAction(okAction)
