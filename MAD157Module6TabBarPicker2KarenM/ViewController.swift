6d29cc7b149f2b92813d01084b0bd95c864d7bde
km <35552602+catnap7777@users.noreply.github.com>
2020-09-21 15:33:54 -0500

    initial commit

--- MAD157Module6TabBarPicker2KarenM/ViewController.swift
+++ MAD157Module6TabBarPicker2KarenM/ViewController.swift
@@ -10,11 +10,40 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var labelResult: UILabel!
+    @IBOutlet var buttonObj: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        labelResult.numberOfLines = 0
+        
+        
     }
 
+    @IBAction func buttonTapped(_ sender: Any) {
+        
+        //.. the alert
+        let alert = UIAlertController(title: "Warning", message: "Zombies are loose!", preferredStyle: .alert)
+        
+        //.. alert action for each "button" on the alert
+        let okAction = UIAlertAction(title: "OK", style: .default, handler: { action -> Void in self .labelResult.text = "OK"         })
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: { action -> Void in self .labelResult.text = "Cancel"         })
+        
+        let destroyAction = UIAlertAction(title: "Destroy", style: .destructive, handler: { action -> Void in self .labelResult.text = "Destroy"         })
+        
+        //.. add the "buttons" to the alert
+        alert.addAction(okAction)
+        alert.addAction(cancelAction)
+        alert.addAction(destroyAction)
+        
+        //.. display the alert
+        self .present(alert, animated: true , completion: nil )
 
+      
+    }
+    
 }
 
